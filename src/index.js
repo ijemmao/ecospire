36ff89bd162b0365ba42bae2f748ae0fd942c98b
ijemmao <ijemmaonwuzulike10@gmail.com>
2019-04-06 15:46:05 -0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import env from './../env.json';
-// import App from './App';
-import Login from './screens/Login';
+import App from './App';
 
 document.getElementsByTagName('meta')[4].setAttribute('content', env.GOOGLE_CLIENT_ID);
-ReactDOM.render(<Login />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
