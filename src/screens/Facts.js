60453e3eb47233199b0077227afc28e96eedf8a0
ijemmao <ijemmaonwuzulike10@gmail.com>
2019-04-06 16:53:13 -0400

    able to get calendar events

--- src/screens/Facts.js
+++ src/screens/Facts.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import Donate from './../components/Donate';
+import env from './../../env.json';
+
+const CLIENT_ID = env.GOOGLE_CLIENT_ID;
+const API_KEY = env.API_KEY;
+const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'];
+const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
 
 export default class Facts extends React.Component {
   constructor(props) {
@@ -10,6 +16,52 @@ export default class Facts extends React.Component {
       // averageComparison: 0,
       // offsetFootprint: 0,
     };
+  }
+
+  componentWillMount = () => {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        apiKey: API_KEY,
+        client_id: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      }).then(() => {
+        window.gapi.auth2.getAuthInstance().signIn().then(() => {
+          window.gapi.client.calendar.events.list({
+            calendarId: 'primary',
+            timeMin: (new Date()).toISOString(),
+            showDeleted: false,
+            singleEvents: true,
+            maxResults: 10,
+            orderBy: 'startTime',
+          }).then((response) => {
+            console.log(response);
+            // const events = response.result.items;
+            // this.appendPre('Upcoming events:');
+
+            // if (events.length > 0) {
+            //   for (let i = 0; i < events.length; i += 1) {
+            //     const event = events[i];
+            //     let when = event.start.dateTime;
+            //     if (!when) {
+            //       when = event.start.date;
+            //     }
+            //     this.appendPre(`${event.summary} (${when})`);
+            //   }
+            // } else {
+            //   this.appendPre('No upcoming events found.');
+            // }
+          });
+        });
+      });
+    });
+  }
+
+  componentDidMount = () => {
+  }
+
+  appendPre = (message) => {
+    console.log(message);
   }
 
   render() {
