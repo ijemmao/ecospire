43035109571bb26bec232c76f3589eddcb47c40a
annie ke <annieke8@gmail.com>
2019-04-06 18:08:51 -0400

    initialized component

---
+++ src/screens/LocationHistory.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class LocationHistory extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div>
+        heyyyyy
+      </div>
+    );
+  }
+}
