283656add8f3e3ec946102ebaa400d09378b4798
Robert He <robert.he.19@dartmouth.edu>
2019-04-06 20:23:45 -0400

    fixed styling of selection

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Dropdown, Menu } from 'semantic-ui-react';
 
-const options = [
-  { key: 1, text: 'Honda', value: 1 },
-  { key: 2, text: 'Volkswagon', value: 2 },
-  { key: 3, text: 'Toyota', value: 3 },
-];
-
 // eslint-disable-next-line react/prefer-stateless-function
 export default class DropdownMenu extends React.Component {
   // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props);
+    this.state = {
+      text: this.props.text,
+      // eslint-disable-next-line react/no-unused-state
+      value: this.props.text,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      text: e.target.value,
+    });
   }
 
   render() {
     return (
       <Menu compact>
-        <Dropdown text={this.props.text} options={options} simple item />
+        <Dropdown text={this.state.text} onChange={this.handleChange} options={this.props.options} simple item />
       </Menu>
     );
   }
--- src/screens/Calculate.js
+++ src/screens/Calculate.js
@@ -2,6 +2,38 @@ import React from 'react';
 import DropdownMenu from '../components/DropdownMenu';
 import ButtonSemantic from '../components/ButtonSemantic';
 
+
+// eslint-disable-next-line no-unused-vars
+const make = [
+  { key: 1, text: 'Honda', value: 1 },
+  { key: 2, text: 'Nissan', value: 2 },
+  { key: 3, text: 'Toyota', value: 3 },
+];
+
+// eslint-disable-next-line no-unused-vars
+const model = [
+  { key: 1, text: 'Accord', value: 1 },
+  { key: 2, text: 'Altima', value: 2 },
+  { key: 3, text: 'Camry', value: 3 },
+];
+
+const year = [
+  { key: 1, text: '2000', value: 1 },
+  { key: 2, text: '2001', value: 2 },
+  { key: 3, text: '2002', value: 3 },
+  { key: 4, text: '2003', value: 4 },
+  { key: 5, text: '2004', value: 5 },
+  { key: 6, text: '2005', value: 6 },
+  { key: 7, text: '2006', value: 7 },
+  { key: 8, text: '2007', value: 8 },
+  { key: 9, text: '2008', value: 9 },
+  { key: 10, text: '2009', value: 10 },
+  { key: 11, text: '2010', value: 11 },
+  { key: 12, text: '2011', value: 12 },
+  { key: 13, text: '2012', value: 13 },
+];
+
+
 // eslint-disable-next-line react/prefer-stateless-function
 export default class Calculate extends React.Component {
   // eslint-disable-next-line no-useless-constructor
@@ -12,12 +44,21 @@ export default class Calculate extends React.Component {
   render() {
     return (
       <div className="calculate-container">
-        <h1>Please provide us with more details on your carbon usage: </h1>
+        <h2>Please provide us with more details on your carbon usage: </h2>
         <p> Make and model of your car </p>
-        <DropdownMenu className="dropdown-container" text="Make" />
-        <DropdownMenu className="dropdown-container" text="Model" />
-        <DropdownMenu className="dropdown-container" text="Year" />
-        <ButtonSemantic text="See Carbon Footprint" />
+        <div className="dropdown-wrapper">
+          <div className="dropdown-container">
+            <DropdownMenu className="dropdown-container" text="Make" options={make} />
+          </div>
+          <div className="dropdown-container">
+            <DropdownMenu className="dropdown-container" text="Model" options={model} />
+          </div>
+          <div className="dropdown-container">
+            <DropdownMenu className="dropdown-container" text="Year" options={year} />
+          </div>
+        </div>
+        <div className="button-container"> <ButtonSemantic text="See Carbon Footprint" />
+        </div>
       </div>
     );
   }
